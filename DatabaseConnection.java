ee8fbcd2eca48012c82cbaed05f2759f890d5484
Viraj Paste <pasteviraj5@gmail.com>
2026-09-07 17:17:51 +0530

    Create DatabaseConnection.java

---
+++ DatabaseConnection.java
@@ -0,0 +1,19 @@
+package com.codescope.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/codescope_ai";
+
+    private static final String USER = "root";
+
+    private static final String PASSWORD = "Your_Password";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
